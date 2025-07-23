4aab837e283a93b7d9bc00aae2781caeec283dd7
Yash__ <placement.gate2028@gmail.com>
2025-07-23 15:03:10 +0530

    Python function to find Minimum and Maximum element in an array

---
+++ MaxMin.py
@@ -0,0 +1,8 @@
+def MaxMin(arr):
+    minimum = min(arr)
+    maximum = max(arr)
+    
+    print(f"The minimum number = {minimum}")
+    print(f"The maximum number = {maximum}")
+    
+MaxMin([1,5,2,7,3])
